1b0a1e9afe0e3544bce6b697b33f8107ffe7470d
Stephen Saunders <saunders.stephen@gmail.com>
2024-05-12 22:33:11 -0700

    test: add unit test for automated test config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use warp::Filter;
+    use warp::test::request;
+
+    #[tokio::test]
+    async fn test_hello_world() {
+        let api = warp::get().map(|| "Hell, world!");
+
+        let resp = request()
+            .method("GET")
+            .path("/")
+            .reply(&api)
+            .await;
+
+        assert_eq!(resp.status(), 200, "Should return 200 OK.");
+        assert_eq!(resp.body(), "Hello, world!", "Should return 'Hello, world!'.");
+    }
+}
